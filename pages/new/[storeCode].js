0a053e5bc2225935cf8025d24f6461119c00d44b
e.ponomarev <e.ponomarev@fun-box.ru>
2021-04-02 14:07:34 +0700

    Attempt to implement static rendering for product list

--- pages/new/[storeCode].js
+++ pages/new/[storeCode].js
@@ -1,56 +1,71 @@
 import { useState, useEffect } from 'react';
-import stores from '../../data/stores.json';
 import productsModel from '../../models/products';
+import storesModel from '../../models/stores';
 import Layout from '../../components/layout';
 import ProductList from '../../components/product-list';
+import Pagination from '../../components/pagination';
 
 const COUNT_ON_PAGE = 20;
 
-export default function New({ store, products }) {
-  const getPageList = (pageNumber) => {
-    const beginIndex = COUNT_ON_PAGE * (pageNumber - 1);
-    const endIndex = COUNT_ON_PAGE * pageNumber;
+export default function New({ stores = [], products = [] }) {
+  const currentStore = stores.find(store => store.isCurrent);
 
-    return products.slice(beginIndex, endIndex);
-  }
+  const [pageList, setPageList] = useState(getPageList(1));
 
   useEffect(() => {
-    const currentStoreCode = localStorage.getItem('store');
-    if (currentStoreCode !== store.code) {
-      localStorage.setItem('store', store.code);
+    const userStoreCode = localStorage.getItem('store');
+
+    if (userStoreCode !== currentStore.code) {
+      localStorage.setItem('store', currentStore.code);
       setPageList(getPageList(1));
     }
   });
 
-  const pagesCount = Math.ceil(products.length / COUNT_ON_PAGE);
-  const [pageList, setPageList] = useState(getPageList(1));
-
   return (
-    <Layout stores={stores} currentStore={store}>
+    <Layout stores={stores} currentStore={currentStore}>
       <ProductList products={pageList}/>
+
+      {/* <Pagination
+        pagesCount={pagesCount}
+        currentPageNum={}
+        onChangePage={pageNum => setPageList(getPageList(pageNum))}
+      /> */}
     </Layout>
   )
 }
 
 export async function getStaticProps({ params }) {
-  const store = stores.find(store => store.code === params.storeCode);
-  const products = productsModel.getList(store.code);
+  const stores = storesModel.getList().map(store => ({
+    ...store,
+    isCurrent: store.code === params.storeCode,
+  }));
+  const products = productsModel.getList(params);
 
   return {
     props: {
-      store,
+      stores,
       products,
     },
+    revalidate: 5,
   };
 }
 
 export async function getStaticPaths() {
-  const paths = stores.map(({ code }) => ({
-    params: { storeCode: code },
-  }));
+  const stores = storesModel.getList();
+  const paths = stores.map((store) => {
+    const storeCode = store.code;
+    const pageNumbersList = productsModel.getPageNumbersList({ storeCode });
+
+    return pageNumbersList.map((pageNum) => ({
+      params: {
+        storeCode,
+        pageNum,
+      }
+    }));
+  }).flat();
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
