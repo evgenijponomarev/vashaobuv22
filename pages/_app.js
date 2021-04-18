3aa57cd36d08a586f67ad23b0755d85e4fcbcb96
e.ponomarev <e.ponomarev@fun-box.ru>
2021-04-19 00:51:00 +0700

    Add YM

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading, react/forbid-prop-types, max-len */
-import Head from 'next/head';
+import Router from 'next/router';
+import withYM from 'next-ym';
 import PropTypes from '../lib/prop-types';
 import styleVars from '../styles/vars';
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        {/* <!-- Yandex.Metrika counter -->
-        <script type="text/javascript" >
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-          ym(76072615, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
-          });
-        </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/76072615" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-        <!-- /Yandex.Metrika counter --> */}
-      </Head>
       <Component {...pageProps}/>
 
       <style jsx global>
@@ -58,3 +43,5 @@ App.propTypes = {
   Component: PropTypes.func.isRequired,
   pageProps: PropTypes.object,
 };
+
+export default withYM('76072615', Router)(App);
