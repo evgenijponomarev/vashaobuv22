8ac93a26fef9dce6f798bffcedd2cd35ad69fbdf
evgenijponomarev <evgenij.ponomarev@icloud.com>
2021-05-11 20:37:14 +0700

    Remove debug and old version uploading

--- pages/api/data.js
+++ pages/api/data.js
@@ -5,18 +5,9 @@ import { updateData } from '../../lib/data';
 
 const actions = {
   POST(req, res) {
-    console.log('DATA UPLOADING REQUEST:\n');
-    console.log(req);
-
     const form = new multiparty.Form();
 
     form.parse(req, async (error, fields, files) => {
-      console.log('DATA UPLOADING ERROR:\n');
-      console.log(error);
-      console.log('DATA UPLOADING FIELDS:\n');
-      console.log(fields);
-      console.log('DATA UPLOADING files:\n');
-      console.log(files);
       const fileData = files.data[0];
       await fsHelpers.saveSourceData(fileData);
       const sourceData = fsHelpers.getSourceData();
